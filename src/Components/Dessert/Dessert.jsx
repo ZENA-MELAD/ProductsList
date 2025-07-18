65753420b804ebe5d0da2d6a3e0df4f6bf24f18b
zena <meladzena23@gmail.com>
2025-07-18 12:08:39 +0300

    update in dessert

--- src/Components/Dessert/Dessert.jsx
+++ src/Components/Dessert/Dessert.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ButtonCard from "../ButtonCard/ButtonCard";
 
-const Dessert = ({ productImage }) => {
+const Dessert = ({ productImage}) => {
   return (
     <div className="w-full relative flex flex-col justify-center items-center mb-8">
       <div className="w-full h-56 xs:h-40  overflow-hidden rounded-lg">
